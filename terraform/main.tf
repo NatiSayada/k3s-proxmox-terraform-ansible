1567ed1d17f366b3cce5ac47350aef36309ac93d
Netanel Sayada <ssnetanel@gmail.com>
2022-03-13 12:31:44 +0200

    copy var file to my-cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,6 +64,11 @@ resource "local_file" "k8s_file" {
   filename = "../inventory/my-cluster/hosts.ini"
 }
 
+resource "local_file" "var_file" {
+  source = "../inventory/sample/group_vars/all.yml"
+  filename = "../inventory/my-cluster/group_vars/all.yml"
+}
+
 output "Master-IPS" {
   value = ["${proxmox_vm_qemu.proxmox_vm_master.*.default_ipv4_address}"]
 }
