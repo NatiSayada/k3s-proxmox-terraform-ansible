057df209f1261c018a710c71f0be514c01164460
Netanel Sayada <ssnetanel@gmail.com>
2022-03-13 13:04:42 +0200

    update indent

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,6 +72,7 @@ resource "local_file" "var_file" {
 output "Master-IPS" {
   value = ["${proxmox_vm_qemu.proxmox_vm_master.*.default_ipv4_address}"]
 }
+
 output "worker-IPS" {
   value = ["${proxmox_vm_qemu.proxmox_vm_workers.*.default_ipv4_address}"]
 }
